4e98bf5683eec3c01c07ef499bbba39b85d0c94e
kasparkannik <kapu7@msn.com>
2015-12-10 23:56:45 +0200

    Lisatud Javadoc kommentaarid

--- src/player/MainClass.java
+++ src/player/MainClass.java
@@ -1,5 +1,4 @@
-package player;
-
+package mediaplayer;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
@@ -18,26 +17,49 @@ public class MainClass {
     FileInputStream fileIn;
     BufferedInputStream bufferIn;
 
+    /**
+     * Player on üks osa javazoom.jl libraryst, saab edastada MPEG audiofaile
+     */
     public Player player;
 
+    /**
+     * Muutuja pauseLoc, mida kasutatakse Stop, Pause, Resume meetodites,
+     * arvutamaks kui palju on muusika kestnud
+     */
     public long pauseLoc;
 
+    /**
+     * Muutuja songLen, mida kasutatakse Stop, Play, Resume meetoditeks, mis
+     * arvutab loo pikkuse
+     */
     public long songLen;
 
+    /**
+     * Muutuja fileLoc, mida kasutatakse Resume, Play meetodis, mis näitab ära
+     * faili asukoha
+     */
     public String fileLoc;
 
+    private int volume;
+
+    /**
+     * Stop meetodi kirjeldus
+     */
     public void Stop() {
         if (player != null) {
             player.close();
-
+//Viib programmi uuesti algseisu tagasi
             pauseLoc = 0;
             songLen = 0;
-
+//Kustutab ekraanilt laulu nime
             MainGui.Display.setText("");
         }
 
     }
 
+    /**
+     * Pause meetodi kirjeldus
+     */
     public void Pause() {
         if (player != null) {
             try {
@@ -49,6 +71,9 @@ public class MainClass {
 
     }
 
+    /**
+     * Resume meetodi funktsioon. Jätkab lugu kohast, kust lugu pausile pandi
+     */
     public void Resume() {
         try {
             fileIn = new FileInputStream(fileLoc);
@@ -72,6 +97,11 @@ public class MainClass {
 
     }
 
+    /**
+     * Play meetodi defineerimine
+     *
+     * @param path, faili asukoht arvutis
+     */
     public void Play(String path) {
         try {
 
@@ -86,17 +116,21 @@ public class MainClass {
         } catch (FileNotFoundException | JavaLayerException ex) {
         } catch (IOException ex) {
         }
-
+        /**
+         * Uus thread, et muusikat saaks kuulata tagataustal
+         */
         new Thread() {
             @Override
             public void run() {
                 try {
                     player.play();
 
+// Kui lugu on lõppenud ja loop on "on" siis taasesitatakse fail 
                     if (player.isComplete() && MainGui.count == 1) {
 
                         Play(fileLoc);
                     }
+// Loo lõppedes, kustutab ekraanilt nime
                     if (player.isComplete()) {
                         MainGui.Display.setText("");
                     }
